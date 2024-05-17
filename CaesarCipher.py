233d5e80a105a74d41d6495a7f117d2937d61437
cooldadpresident <bumabuch@gmail.com>
2024-05-17 09:56:43 +0200

    completed CaesarCipher encryption

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,16 +1,29 @@
-#inicialization of variables
-alphabet = 'abcdef'
-print('string', alphabet)
+'''
+Caesars cipher
+'''
+plaintext = input('Message to cipher ')
+
+#Convert the plaintext string to lowercase to ensure consistency.
+
+plaintext = plaintext.lower()
+print('Plaintext in lowercase', plaintext)
 message = ''
-rotation = 1
+key = 3
 
-# cycle for each character
-for char in alphabet:
+    
+
+# iterate through each character in the plaintext string.
+
+for char in plaintext:
     i = ord(char)
-    i = i + rotation
-    char = chr(i)
-    message = message + char
-# output
-print("ciphered message", message)
-input()
+    i = i + key
+    if (i > ord('z')):
+        i = i - 26
+    # shift the ASCII value of the character by the key value (3 in this case).
 
+    char = chr(i)
+     # Append the encrypted character to the message string.
+    message = message + char
+    
+# output
+print('Ciphertext', message)
